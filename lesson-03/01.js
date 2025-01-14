2bf885bcb47aa54c654cc2261c4d207e3f3b01de
Shukevich Alexandr <shukevich.alexandr@gmail.com>
2025-01-14 20:07:32 +0300

    tusk-7

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,10 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+
+
+function calculateAverage(x, y) {
+    return (x + y) / 2;
+}
+
+// const result = calculateAverage(2,4);
+// alert(result);
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,15 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(...numbers) {
+let largest = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > largest) {
+        largest = numbers[i];
+    }
+    }
+    return largest;
+}
+
+// const result = findLargest(1, 3, 5, 7, 9, 11);
+// alert(result)
